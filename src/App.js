b1be3ae6461ca0557d964be5b7c55355968bae92
Rumble <rumble@Admins-MacBook-Pro.local>
2024-11-12 17:35:26 +0000

    index ammended

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage/Home';
 import Footer from './Components/Footer/Footer';
-import Navbar from './Components/NavBar/NavBarComponent';
-import ImageLoader from './Components/ImageLoaderComponent/ImageLoader';
 import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                    </Routes>
-                </header>
-                <Footer /> {/* Include Footer */}
-            </div>
-        </Router>
-    );
+  return (
+    <Router basename="/LeamonOsteopathy"> {/* Add basename here */}
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </header>
+        <Footer /> {/* Include Footer */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
