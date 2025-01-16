dadba769bf69ede190cfe5482710de0fa8fcb59e
Rumble <rumble@Admins-MacBook-Pro.local>
2025-01-16 14:20:51 +0000

    carousel slowed down on request

--- src/Components/TestimonialCarousel/TestimonialCarousel.jsx
+++ src/Components/TestimonialCarousel/TestimonialCarousel.jsx
@@ -39,7 +39,7 @@ const TestimonialCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2500,
+    autoplaySpeed: 3650,
     arrows: false,
   };
 
