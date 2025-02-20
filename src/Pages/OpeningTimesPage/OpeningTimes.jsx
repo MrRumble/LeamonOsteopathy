e3538eb41fddec3fa5eb62bf6e39a26321a845e6
Rumble <rumble@Admins-MacBook-Pro.local>
2025-02-20 16:57:16 +0000

    opening times updated, cancelation policy added.

--- src/Pages/OpeningTimesPage/OpeningTimes.jsx
+++ src/Pages/OpeningTimesPage/OpeningTimes.jsx
@@ -22,18 +22,17 @@ const OpeningTimesPrices = () => {
                         <h2>Herstmonceux</h2>
                             <ul className="list">
                                 <li className="listItem">Monday: 8.30am - 7.00pm</li>
-                                <li className="listItem">Thursday: 8.00am - 12.00pm</li>
+                                <li className="listItem">Thursday: 7.00am - 12.00pm</li>
                                 <li className="listItem">Friday: 8.30am - 7.00pm</li>
                             </ul>
                         <p>
-                        New Patient: £42 , Follow Up: £40
+                        New Patient: £45 , Follow Up: £40
                         </p>
                         <div className="qualifications">
                             <h2>Mayfield</h2>
                             <ul className="list">
                                 <li className="listItem">Tuesday: 9.00am - 7.00pm</li>
                                 <li className="listItem">Wednesday: 9.00am - 7.00pm</li>
-                                <li className="listItem">Thursday: 1.00pm - 7.00pm</li>
                             </ul>
                             <p>
                                 New Patient: £65 , Follow Up: £60
